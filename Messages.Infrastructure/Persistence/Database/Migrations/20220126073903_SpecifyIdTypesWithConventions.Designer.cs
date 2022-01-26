60e0c4ca8c6265a47678ca3dda0020f448b16697
tnotheis <timo.notheisen@js-soft.com>
2022-01-26 08:49:08 +0100

    chore: add migration for changes

--- Messages.Infrastructure/Persistence/Database/Migrations/20220126073903_SpecifyIdTypesWithConventions.Designer.cs
+++ Messages.Infrastructure/Persistence/Database/Migrations/20220126073903_SpecifyIdTypesWithConventions.Designer.cs
@@ -4,6 +4,7 @@ using Messages.Infrastructure.Persistence.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Messages.Infrastructure.Persistence.Database.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220126073903_SpecifyIdTypesWithConventions")]
+    partial class SpecifyIdTypesWithConventions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
